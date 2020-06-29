e9f24d46a8527a2f6d415ca6b6f4f659e9e2d85c
ChaosGuru <chaos.feature@gmail.com>
2020-06-29 23:48:48 +0300

    Now bot know how to respond

--- main.py
+++ main.py
@@ -5,28 +5,38 @@ Give you 10 reandom pictures based on your input text.
 """
 
 import requests as rqs
-import telegram
 import json
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
 
-# class Bot:
-#     def __init__(self, token):
-#         self.bot = r'https://api.telegram.org/bot' + token
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="F***ing freak, what i need to find this time!!?")
 
-#     def getMe(self):
-#         response = rqs.get(self.bot + '/getMe').text
-#         return json.loads(response)
+def echo(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
 def main():
     # get tocken
     with open('token.json') as f:
         token = json.load(f)["token"]
 
-    # get bot
-    bot = telegram.Bot(token=token)
-    print(bot.get_me())
+    # set logging
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-    # TenPicsBot = Bot(token)
-    # print(TenPicsBot.getMe())
+    # get updater and dispatcher
+    updater = Updater(token=token, use_context=True)
+    dispatcher = updater.dispatcher
+
+    # set command handler
+    start_handler = CommandHandler('start', start)
+    dispatcher.add_handler(start_handler)
+
+    # set message handler
+    echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+    dispatcher.add_handler(echo_handler)
+
+    # start the bot
+    updater.start_polling()
 
 if __name__ == '__main__':
     main()
